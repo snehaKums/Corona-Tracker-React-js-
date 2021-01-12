2b860fac10f041095f5341f115cf1516ff7ac33e
Sneha Kumari <snehakumari874@gmail.com>
2021-01-12 17:12:37 +0530

    Updates

---
+++ precache-manifest.0d081d09f747faf4e32811157097baca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb305b9091b4877ad268e5f9cc176334",
+    "url": "/Corona-Tracker-React-js-/index.html"
+  },
+  {
+    "revision": "ef5c3f3f6b574dadb729",
+    "url": "/Corona-Tracker-React-js-/static/css/main.1fd378ff.chunk.css"
+  },
+  {
+    "revision": "c4e5047030b7bc98e6b5",
+    "url": "/Corona-Tracker-React-js-/static/js/2.73393128.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Corona-Tracker-React-js-/static/js/2.73393128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5c3f3f6b574dadb729",
+    "url": "/Corona-Tracker-React-js-/static/js/main.a2f19c6a.chunk.js"
+  },
+  {
+    "revision": "919530b2c0c788d06ab3",
+    "url": "/Corona-Tracker-React-js-/static/js/runtime-main.209ea6b8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Tracker-React-js-/static/media/image.d7265326.png"
+  }
+]);
